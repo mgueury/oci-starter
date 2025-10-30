1c21232dc9aede8eac916ce5c0cb4d133bdf3fb1
Marc Gueury <marc.gueury@oracle.com>
2025-10-30 17:41:34 +0100

    v4.0

--- basis/src/app/src/env.j2.sh
+++ basis/src/app/src/env.j2.sh
@@ -2,5 +2,5 @@
 # The values between ##xxx## will be filled during build.
 
 # Database
-{% import "start_sh.j2_macro" as m with context %}
+{%- import "start_sh.j2_macro" as m with context %}
 {{ m.env() }}
