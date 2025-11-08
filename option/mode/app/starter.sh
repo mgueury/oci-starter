6e02c21264e6bacfd35df60a897d3271235a7e95
Marc Gueury <marc.gueury@oracle.com>
2025-11-08 23:01:04 +0100

    v4.0

--- option/mode/app/starter.sh
+++ option/mode/app/starter.sh
@@ -4,10 +4,22 @@ cd $SCRIPT_DIR/..
 
 STARTER_DIR=".starter"
 if [ -f $STARTER_DIR/starter.sh ]; then
-  echo "Copying the source files to: starter"
-  rsync app -av $STARTER_DIR/src/app/ .
+  echo "Synchronizing the source files with directory $STARTER_DIR"
+  if [ -d app ]; then
+    rsync -av app $STARTER_DIR/src/app
+  fi
+  if [ -d src ]; then
+    rsync -av src $STARTER_DIR/src/app/src
+    if [ -f build_app.sh ]; then
+      cp build_app.sh $STARTER_DIR/src/app/.
+    fi
+  fi
   if [ -d db ]; then
-    rsync db -av $STARTER_DIR/src/db/ .
+    rsync -av db $STARTER_DIR/src/db
+  fi
+  if [ -f terraform ]; then
+    rsync -R terraform $STARTER_DIR/target/app_terraform
+    cp -R $STARTER_DIR/target/app_terraform $STARTER_DIR/src/terraform
   fi
   if [ -f terraform.tfvars ]; then
     cp terraform.tfvars $STARTER_DIR
@@ -18,6 +30,6 @@ if [ -f $STARTER_DIR/starter.sh ]; then
   # cp done.txt starter/.
   $STARTER_DIR/starter.sh $@
 else
-  echo "Error: starter directory not present"
+  echo "Error: $STARTER_DIR directory is missing"
 fi  
 exit ${PIPESTATUS[0]}
