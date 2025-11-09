58a8825f063e8aa09070351fadb3116ed00c50ed
Marc Gueury <marc.gueury@oracle.com>
2025-11-09 01:05:43 +0100

    v4.0

--- option/mode/app/starter.sh
+++ option/mode/app/starter.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR/..
+cd $SCRIPT_DIR
 
 STARTER_DIR=".starter"
 if [ -f $STARTER_DIR/starter.sh ]; then
