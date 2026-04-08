487904962616e58b018499f7f623c9d8fcd115cb
Marc Gueury <marc.gueury@oracle.com>
2026-04-08 19:39:40 +0200

    v4.0

--- basis/bin/compute/rebuild.sh
+++ basis/bin/compute/rebuild.sh
@@ -3,26 +3,24 @@ cd $SCRIPT_DIR
 
 . ./shared_compute.sh
 
-TARGET_OKE="/tmp/oke"
+TARGET_OKE="$HOME/target/oke"
 mkdir -p $TARGET_OKE
 
-if [ "$TF_VAR_deploy_type" == "public_compute" ] || [ "$TF_VAR_deploy_type" == "private_compute" ] || [ "$TF_VAR_deploy_type" == "instance_pool" ]; then
-    for APP_DIR in `app_dir_list`; do
-        if [ -f $APP_DIR/install.sh ]; then
-            title "$APP_DIR: Install"
-            ${APP_DIR}/install.sh
-            if [ -f ${APP_DIR}/restart.sh ]; then
-                ${APP_DIR}/restart.sh
-            fi
-        fi  
-    done
-elif [ "$TF_VAR_deploy_type" == "kubernetes" ] ; then 
-    cd $HOME/app
-    for APP_DIR in `app_dir_list`; do
-        if [ -f $APP_DIR/Dockerfile ]; then
-            APP_NAME=$(basename "${APP_DIR}")
+cd $HOME/app
+
+for APP_DIR in `app_dir_list`; do
+    if [ -f $APP_DIR/install.sh ]; then
+        APP_NAME=$(basename "${APP_DIR}")
+        if [ -f build_${APP_NAME}.sh]; then
             title "$APP_NAME: Build"
             ./build_${APP_NAME}.sh
+        fi
+        if [ "$TF_VAR_deploy_type" == "public_compute" ] || [ "$TF_VAR_deploy_type" == "private_compute" ] || [ "$TF_VAR_deploy_type" == "instance_pool" ]; then
+            if [ -f ${APP_DIR}/restart.sh ]; then
+                title "$APP_NAME: Restart"
+                ${APP_DIR}/restart.sh
+            fi
+        elif [ "$TF_VAR_deploy_type" == "kubernetes" ] ; then 
             title "$APP_NAME: Deploy in OKE"
             if [ -f k8s.yaml ]; then
                 copy_replace_apply_target_oke src/app/${APP_NAME}/k8s.yaml
@@ -30,8 +28,8 @@ elif [ "$TF_VAR_deploy_type" == "kubernetes" ] ; then
             if [ -f k8s-ingress.yaml ]; then
                 copy_replace_apply_target_oke src/app/${APP_NAME}/k8s-ingress.yaml
             fi
-        fi  
-    done
-else 
-    echo "rebuild.sh: TF_VAR_deploy_type: $TF_VAR_deploy_type is not supported. It requires terraform to reexecute."
-fi
+        else 
+            echo "rebuild.sh: TF_VAR_deploy_type: $TF_VAR_deploy_type is not supported. It requires terraform to redeploy."
+        fi
+    fi  
+done
