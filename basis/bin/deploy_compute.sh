8eb2fa4bccf918353bb7920218be4628dca4311c
Marc Gueury <marc.gueury@oracle.com>
2026-04-12 13:13:58 +0200

    v4.0

--- basis/bin/deploy_compute.sh
+++ basis/bin/deploy_compute.sh
@@ -8,7 +8,12 @@ cd $PROJECT_DIR
 
 echo "COMPUTE_IP=$COMPUTE_IP"
 
-cp $TARGET_DIR/tf_env.sh target/compute/compute/.
+# Create the target/compute directory
+$COMPUTE_DIR=$TARGET_DIR/compute
+mkdir -p $COMPUTE_DIR
+cp -R $BIN_DIR/compute $COMPUTE_DIR/.
+cp $TARGET_DIR/tf_env.sh $COMPUTE_DIR/compute/.
+
 scp_via_bastion "target/compute/*" opc@$COMPUTE_IP:/home/opc/.
 ssh -o StrictHostKeyChecking=no -oProxyCommand="$BASTION_PROXY_COMMAND" opc@$COMPUTE_IP "bash compute/compute_install.sh 2>&1 | tee -a compute/compute_install.log"
 exit_on_error "Deploy Compute - ssh"
