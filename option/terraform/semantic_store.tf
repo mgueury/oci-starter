77fe4756495e5dc86a1bf957ec5aa17d13798272
Marc Gueury <marc.gueury@oracle.com>
2026-06-24 16:02:11 +0200

    v5.0

--- option/terraform/semantic_store.tf
+++ option/terraform/semantic_store.tf
@@ -104,12 +104,22 @@ output "private_endpoint_d" {
 
 // -- Connections ------------------------------------------------------------
 
+locals {
+  # Replace the hostname with the IP in the connection string (else KB creation will give Internal Server Error)
+  db_url_ip1 = replace(local.local_db_url, data.oci_database_autonomous_database.starter_atp.private_endpoint, data.oci_database_autonomous_database.starter_atp.private_endpoint_ip)
+  db_url_ip = replace(local.db_url_ip1, "retry_count=20", "retry_count=3")
+}
+
+output "db_url_ip" {
+  value = local.db_url_ip
+}
+
 # Connection - Resource
 resource "oci_database_tools_database_tools_connection" "starter_dbtools_connection" {
   compartment_id    = local.lz_db_cmp_ocid
   display_name      = "${var.prefix}-dbtools-connection"
   type              = "ORACLE_DATABASE"
-  connection_string = local.local_db_url
+  connection_string = local.db_url_ip
   user_name         = "admin"
   user_password {
     value_type = "SECRETID"
