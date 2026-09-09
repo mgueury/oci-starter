ade2e092158e8a204d7d40df0e896b62c42ef811
Marc Gueury <92148382+MarcGueury@users.noreply.github.com>
2026-09-09 23:30:08 +0200

    v5.0

--- py_oci_starter.py
+++ py_oci_starter.py
@@ -1319,7 +1319,6 @@ def jinja2_replace_template():
     ui_param = jinja2_ui_params.get( params.get('ui_type') )
     if ui_param is not None:
         template_param = {**template_param, **ui_param}
-    else 
     print( "debug1 - " + params.get('ui_type'))
     print( "debug2 - " + template_param.get('ui_name'))
 
