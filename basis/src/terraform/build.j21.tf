fe9e30981a76796ef0c306ec020798ba519af5ed
Marc Gueury <92148382+MarcGueury@users.noreply.github.com>
2026-09-21 11:48:31 +0200

    v5.0

--- basis/src/terraform/build.j21.tf
+++ basis/src/terraform/build.j21.tf
@@ -31,7 +31,8 @@ resource "null_resource" "tf_env" {
     }
     append ""
     append "# Terraform Variables"
-    append ". ./tf_vars.sh"
+    append 'SCRIPT_DIR=$( cd -- "$( dirname -- "\$BASH_SOURCE[0]" )" &> /dev/null && pwd )'
+    append ". \$SCRIPT_DIR/tf_vars.sh"
     append "# Terraform Locals"
 {%- for key in terraform_locals %}
     append_export "{{key.upper()}}" "${local.local_{{key}}}"
